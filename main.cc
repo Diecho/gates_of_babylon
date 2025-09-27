5e0d42f74b88c873888379fa5e73b572341bf5a4
diecho <0975101@my.scccd.edu>
2025-09-27 16:48:46 -0700

    Logged Homework Date: Sat Sep 27 04:48:46 PM PDT 2025

--- main.cc
+++ main.cc
@@ -128,7 +128,7 @@ int main(){
 			cout << "activated" << endl;
 			j--;
 		}else {
-			cout << Gates[i + j].getType() << endl; 
+			cout << Gates[i].getType() << endl; 
 			//cout << "SECONDactivated" << endl;
 			Gates[i + j].setConnectedTo(total - j);	
 			Gate g(6);
